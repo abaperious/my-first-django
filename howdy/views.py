2a9564ecfd325bd9147f318c578a953dabf05bd4
Maciej Nowak <feather.pl@gmail.com>
2017-07-05 12:20:47 +0200

    Add post template

--- howdy/views.py
+++ howdy/views.py
@@ -1,6 +1,9 @@
  # howdy/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -12,4 +15,6 @@ class AboutPageView(TemplateView):
     template_name = "about.html"
 
 def post_list(request):
-    return render(request, 'howdy/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'howdy/post_list.html', {'posts': posts})
+    
